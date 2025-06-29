28a2fe4fac9ae216ce5fad9e0b60f0914eb4160e
woctordho <woctordho@outlook.com>
2025-06-29 14:26:14 +0800

    Add example to train a tiny model

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-Work in progress.
+Work in progress. Still needs a lot of optimization.
 
 Currently focus on fine-tuning on a single GPU, while being compatible with the HF Transformers ecosystem.
---
+++ example_train_tiny.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python3
+
+import torch
+from datasets import Dataset
+from peft import LoraConfig, get_peft_model
+from trl import SFTConfig, SFTTrainer
+
+from qwen3_moe_fused.configuration_qwen3_moe_fused import Qwen3MoeFusedConfig
+from qwen3_moe_fused.lora import LoraMoeFusedLinear
+from qwen3_moe_fused.modular_qwen3_moe_fused import (
+    MoeFusedLinear,
+    Qwen3MoeFusedForCausalLM,
+)
+from transformers import AutoTokenizer, BitsAndBytesConfig
+
+
+def main():
+    model_dir = "./pretrained/qwen-moe-tiny-lm"
+
+    config = Qwen3MoeFusedConfig(
+        hidden_size=16,
+        intermediate_size=5,
+        num_hidden_layers=2,
+        num_attention_heads=8,
+        num_key_value_heads=4,
+        max_window_layers=2,
+        moe_intermediate_size=3,
+        num_experts=9,
+        norm_topk_prob=True,
+    )
+    model = Qwen3MoeFusedForCausalLM(config)
+    model.save_pretrained(model_dir)
+
+    bnb_config = BitsAndBytesConfig(load_in_4bit=True, bnb_4bit_compute_dtype=torch.bfloat16)
+    model = Qwen3MoeFusedForCausalLM.from_pretrained(model_dir, quantization_config=bnb_config)
+
+    lora_config_kwargs = {
+        "target_modules": [
+            "q_proj",
+            "k_proj",
+            "v_proj",
+            "o_proj",
+            "gate",
+            "gate_proj",
+            "up_proj",
+            "down_proj",
+        ],
+        "rank_pattern": {
+            "q_proj": 16,
+            "k_proj": 16,
+            "v_proj": 16,
+            "o_proj": 16,
+            "gate": 16,
+            "gate_proj": 4,
+            "up_proj": 4,
+            "down_proj": 4,
+        },
+        "lora_alpha": 1,
+        "use_rslora": True,
+    }
+    lora_config = LoraConfig(**lora_config_kwargs)
+    lora_config._register_custom_module({MoeFusedLinear: LoraMoeFusedLinear})
+    model = get_peft_model(model, lora_config)
+
+    tokenizer = AutoTokenizer.from_pretrained("Qwen/Qwen3-0.6B")
+
+    dataset = Dataset.from_dict(
+        {
+            "text": [
+                "a" * 100,
+                "b" * 100,
+                "c" * 100,
+                "d" * 100,
+            ]
+        }
+    )
+
+    sft_config = SFTConfig(
+        per_device_train_batch_size=2,
+        gradient_accumulation_steps=1,
+        learning_rate=1e-2,
+        weight_decay=1e-2,
+        num_train_epochs=1,
+        logging_steps=1,
+        bf16=True,
+        optim="adamw_8bit",
+        dataset_num_proc=1,
+        max_length=1024,
+        report_to="none",
+    )
+    trainer = SFTTrainer(
+        model=model,
+        processing_class=tokenizer,
+        train_dataset=dataset,
+        args=sft_config,
+    )
+
+    trainer_stats = trainer.train()
+    print("trainer_stats")
+    print(trainer_stats)
+
+
+if __name__ == "__main__":
+    main()
