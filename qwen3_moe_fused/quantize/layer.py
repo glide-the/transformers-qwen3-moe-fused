e946a4e5702940408d04974159aa19f15aa548b8
woctordho <woctordho@outlook.com>
2025-07-06 17:17:28 +0800

    Dequantize weight to input.dtype

--- qwen3_moe_fused/quantize/layer.py
+++ qwen3_moe_fused/quantize/layer.py
@@ -16,7 +16,8 @@ from ..modular_qwen3_moe_fused import MoeFusedLinear
 # TODO: Fuse this
 def moe_fused_linear_4bit(input: torch.Tensor, weight: Params4bit, m_sizes: torch.Tensor) -> torch.Tensor:
     assert not weight.requires_grad
-    weight = dequantize_4bit(weight, weight.quant_state).to(input.dtype)
+    weight.quant_state.dtype = input.dtype
+    weight = dequantize_4bit(weight, weight.quant_state)
     return moe_fused_linear(input, weight, m_sizes)
 
 
