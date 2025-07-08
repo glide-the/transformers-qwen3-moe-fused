33651ffd2f9a34983db22cbcddbd0b1a1cdb3e42
woctordho <woctordho@outlook.com>
2025-07-08 10:56:24 +0800

    Add a safer non-inplace implementation of silu_mul

--- qwen3_moe_fused/kernels/silu_mul.py
+++ qwen3_moe_fused/kernels/silu_mul.py
@@ -1,5 +1,4 @@
 # h = silu(e) * g
-# Modified from https://github.com/unslothai/unsloth/blob/01c5e1a24935b93d9d3197815ad71751d9dfb37a/unsloth/kernels/swiglu.py
 
 import torch
 import triton
@@ -21,10 +20,10 @@ _autotune_configs = [
     key=["n_elements"],
 )
 @triton.jit
-def _fg_kernel(
-    e,
-    g,
-    h,
+def _silu_mul_forward_kernel(
+    e_ptr,
+    g_ptr,
+    h_ptr,
     n_elements,
     BLOCK_SIZE: tl.constexpr = 128,
 ):
@@ -32,17 +31,14 @@ def _fg_kernel(
     offsets = block_idx * BLOCK_SIZE + tl.arange(0, BLOCK_SIZE)
     mask = offsets < n_elements
 
-    e_row = tl.load(e + offsets, mask=mask, other=0).to(tl.float32)
-    g_row = tl.load(g + offsets, mask=mask, other=0)  # .to(tl.float32)
+    e = tl.load(e_ptr + offsets, mask=mask).to(tl.float32)
+    g = tl.load(g_ptr + offsets, mask=mask)
 
-    # f = e * sigmoid(e)
-    f_row = e_row * tl.sigmoid(e_row)  # e_row / (1 + tl.exp(-e_row))
-    f_row = f_row.to(g_row.dtype)  # Exact copy from HF
-    # h = f * g
-    h_row = f_row * g_row
+    f = e * tl.sigmoid(e)  # f32
+    f = f.to(e_ptr.dtype.element_ty)
+    h = f * g
 
-    # Store h
-    tl.store(h + offsets, h_row, mask=mask)
+    tl.store(h_ptr + offsets, h, mask=mask)
 
 
 @triton.autotune(
@@ -50,87 +46,129 @@ def _fg_kernel(
     key=["n_elements"],
 )
 @triton.jit
-def _DWf_DW_dfg_kernel(
-    DW,
-    e,
-    g,
+def _silu_mul_backward_kernel(
+    dh_ptr,
+    e_ptr,
+    g_ptr,
+    de_ptr,
+    dg_ptr,
     n_elements,
     BLOCK_SIZE: tl.constexpr = 128,
 ):
-    """
-    e = e.float()
-    se = 1.0 / (1.0 + torch.exp(-e))
-    f = (se * e).to(dtype)
-    h = f * g
-    df = DW * f
-    dg = DW * g
-    de = (dg.float() * se * (1.0 + e * (1.0 - se))).to(dtype)
-    """
     block_idx = tl.program_id(0)
     offsets = block_idx * BLOCK_SIZE + tl.arange(0, BLOCK_SIZE)
     mask = offsets < n_elements
 
-    DW_row = tl.load(DW + offsets, mask=mask, other=0)  # .to(tl.float32)
-    e_row = tl.load(e + offsets, mask=mask, other=0).to(tl.float32)
-    g_row = tl.load(g + offsets, mask=mask, other=0)  # .to(tl.float32)
+    dh = tl.load(dh_ptr + offsets, mask=mask)
+    e = tl.load(e_ptr + offsets, mask=mask).to(tl.float32)
+    g = tl.load(g_ptr + offsets, mask=mask)
 
-    # e = e.float()
-    # se = 1.0 / (1.0 + torch.exp(-e))
-    se_row = tl.sigmoid(e_row)  # 1.0 / (1.0 + tl.exp(-e_row))
-    # f = (se * e).to(dtype)
-    f_row = se_row * e_row
-    f_row = f_row.to(DW_row.dtype)
-    # h = f * g
-    # h_row = f_row * g_row
-    # df = DW * f
-    df_row = DW_row * f_row
-    # dg = DW * g
-    dg_row = DW_row * g_row
-    # de = (dg.float() * se * (1.0 + e * (1.0 - se))).to(dtype)
-    de_row = dg_row.to(tl.float32) * se_row * (1.0 + e_row * (1.0 - se_row))
-    de_row = de_row.to(DW_row.dtype)
+    se = tl.sigmoid(e)  # f32
+    ese = e * se  # f32
+    f = ese.to(e_ptr.dtype.element_ty)
+    df = dh * g
+    dg = f * dh
 
-    # Store derivatives in buffers
-    # tl.store(DW + offsets, h_row, mask=mask)  # h  = f * g
-    tl.store(e + offsets, df_row, mask=mask)  # df = DW * f
-    tl.store(g + offsets, de_row, mask=mask)  # de
+    de = df.to(tl.float32) * se * (1.0 + e - ese)  # f32
+    de = de.to(de_ptr.dtype.element_ty)
+
+    tl.store(de_ptr + offsets, de, mask=mask)
+    tl.store(dg_ptr + offsets, dg, mask=mask)
+
+
+@triton.autotune(
+    configs=_autotune_configs,
+    key=["n_elements"],
+)
+@triton.jit
+def _silu_mul_backward_inplace_kernel(
+    dh_ptr,
+    e_ptr,
+    g_ptr,
+    n_elements,
+    BLOCK_SIZE: tl.constexpr = 128,
+):
+    block_idx = tl.program_id(0)
+    offsets = block_idx * BLOCK_SIZE + tl.arange(0, BLOCK_SIZE)
+    mask = offsets < n_elements
+
+    dh = tl.load(dh_ptr + offsets, mask=mask)
+    e = tl.load(e_ptr + offsets, mask=mask).to(tl.float32)
+    g = tl.load(g_ptr + offsets, mask=mask)
+
+    se = tl.sigmoid(e)  # f32
+    ese = e * se  # f32
+    f = ese.to(e_ptr.dtype.element_ty)
+    h = f * g
+    df = dh * g
+    dg = f * dh
+
+    de = df.to(tl.float32) * se * (1.0 + e - ese)  # f32
+    de = de.to(e_ptr.dtype.element_ty)
+
+    tl.store(dh_ptr + offsets, h, mask=mask)
+    tl.store(e_ptr + offsets, de, mask=mask)
+    tl.store(g_ptr + offsets, dg, mask=mask)
+
+
+def silu_mul_forward(e: torch.Tensor, g: torch.Tensor) -> torch.Tensor:
+    assert e.is_cuda
+    assert g.device == e.device
+    assert e.is_contiguous()
+    assert g.is_contiguous()
+    assert g.numel() == e.numel()
+
+    n_elements = e.numel()
+    h = torch.empty_like(e)
+    grid = lambda META: (triton.cdiv(n_elements, META["BLOCK_SIZE"]),)
+    _silu_mul_forward_kernel[grid](e, g, h, n_elements)
+    return h
+
+
+def silu_mul_backward(dh: torch.Tensor, e: torch.Tensor, g: torch.Tensor) -> tuple[torch.Tensor, torch.Tensor]:
+    assert e.is_cuda
+    assert g.device == e.device
+    assert dh.device == e.device
+    assert e.is_contiguous()
+    assert g.is_contiguous()
+    assert dh.is_contiguous()
+    assert g.numel() == e.numel()
+    assert dh.numel() == e.numel()
+
+    n_elements = e.numel()
+    de = torch.empty_like(e)
+    dg = torch.empty_like(g)
+    grid = lambda META: (triton.cdiv(n_elements, META["BLOCK_SIZE"]),)
+    _silu_mul_backward_kernel[grid](dh, e, g, de, dg, n_elements)
+    return de, dg
+
+
+# dh, e, g are modified in place to h, de, dg
+def silu_mul_backward_inplace(dh: torch.Tensor, e: torch.Tensor, g: torch.Tensor) -> None:
+    assert e.is_cuda
+    assert g.device == e.device
+    assert dh.device == e.device
+    assert e.is_contiguous()
+    assert g.is_contiguous()
+    assert dh.is_contiguous()
+    assert g.numel() == e.numel()
+    assert dh.numel() == e.numel()
+
+    n_elements = e.numel()
+    grid = lambda META: (triton.cdiv(n_elements, META["BLOCK_SIZE"]),)
+    _silu_mul_backward_inplace_kernel[grid](dh, e, g, n_elements)
 
 
 class SiluMul(torch.autograd.Function):
     @staticmethod
     def forward(ctx, e, g):
-        assert e.is_cuda
-        assert g.device == e.device
-        assert e.is_contiguous()
-        assert g.is_contiguous()
-        assert g.numel() == e.numel()
-
         ctx.save_for_backward(e, g)
-
-        n_elements = e.numel()
-        h = torch.empty_like(e)
-        grid = lambda META: (triton.cdiv(n_elements, META["BLOCK_SIZE"]),)
-        _fg_kernel[grid](e, g, h, n_elements)
-        return h
+        return silu_mul_forward(e, g)
 
     @staticmethod
-    def backward(ctx, grad_h):
+    def backward(ctx, dh):
         e, g = ctx.saved_tensors
-
-        assert e.is_cuda
-        assert g.device == e.device
-        assert grad_h.device == e.device
-        assert e.is_contiguous()
-        assert g.is_contiguous()
-        assert grad_h.is_contiguous()
-        assert g.numel() == e.numel()
-        assert grad_h.numel() == e.numel()
-
-        n_elements = e.numel()
-        # e, g are modified in place to grad_e, grad_g
-        grid = lambda META: (triton.cdiv(n_elements, META["BLOCK_SIZE"]),)
-        _DWf_DW_dfg_kernel[grid](grad_h, e, g, n_elements)
-        return e, g
+        return silu_mul_backward(dh, e, g)
 
 
 silu_mul = SiluMul.apply
